aad1348ab55e08352a47340c2ad8d1083c0b972f
sergiJimenez <acasualcolumbus@gmail.com>
2023-03-15 22:11:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var calculator_1 = require("myfirsttypescriptlibrary/lib/calculator");
+var calculator = new calculator_1.Calculator();
+console.log(calculator.addNumbers(2, 2));
+console.log(calculator.substractNumbers(2, 2));
+console.log(calculator.multiplyNumbers(2, 2));
+console.log(calculator.divideNumbers(2, 2));
---
+++ index.ts
@@ -0,0 +1,8 @@
+import { Calculator } from 'myfirsttypescriptlibrary/lib/calculator';
+
+const calculator = new Calculator();
+
+console.log(calculator.addNumbers(2,2));
+console.log(calculator.substractNumbers(2,2));
+console.log(calculator.multiplyNumbers(2,2));
+console.log(calculator.divideNumbers(2,2));
